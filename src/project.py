9af3d87062538fc93b44fe21e438c5fefa5c5f32
eliaroller <lotfielia@gmail.com>
2025-12-06 01:02:19 -0600

    Added my 3 main classes and set up the rgb values for the base colors of both the lightbulb and ball color and a system to blend the colors to create a realistic shading effect.

---
+++ src/project.py
@@ -0,0 +1,70 @@
+import pygame
+
+import sys
+
+class application:
+
+    def __init__(self, light = ((150, 200), 40), sphere = ((350, 200), 60)):
+
+        pygame.init()
+
+        self.screen = pygame.display.set_mode((600, 400))
+
+        pygame.display.set_caption("Color Theory Visualizer")
+
+        self.clock = pygame.time.Clock()
+
+        self.light = light
+
+        self.sphere = sphere
+
+class lightbulb:
+
+    def __init__(self, position, radius, color = (255, 255, 255)):
+
+        self.position = position
+
+        self.radius = radius
+
+        self.color = color
+
+    def self_color(self, color):
+
+        self.color = color
+
+    def draw(self, screen):
+
+        pygame.draw.circle(screen, self.color, self.position, self.radius)
+
+class ball:
+
+    def __init__(self, position, radius, base_color = (150, 150, 150)):
+
+        self.position = position
+
+        self.radius = radius
+
+        self.base_color = base_color
+
+        self.shaded_color = base_color
+
+    def light_applied(self, light_color):
+
+        r = min(255, self.base_color[0] * light_color[0] // 255)
+
+        g = min(255, self.base_color[1] * light_color[1] // 255)
+
+        b = min(255, self.base_color[2] * light_color[2] // 255)
+
+        self.shaded_color = (r, g, b)
+
+    def draw(self, screen):
+
+        pygame.draw.circle(screen, self.shaded_color, self.position, self.radius)
+
+
+
+
+def main():
+
+    visual = application()
